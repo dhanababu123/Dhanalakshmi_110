9095389cbec59fa38ed4cb628e88d47abe841101
dhanababu123 <dhanababu118@gmail.com>
2022-01-12 20:21:29 +0530

    new react files

---
+++ React/quizapp/src/data.js
@@ -0,0 +1,91 @@
+const data = [
+  {
+    question: "Which of the following is the correct name of React.js?",
+    incorrectAnswers: [
+      "React",
+      "React.js",
+    "ReactJS",
+    ],
+    correctAnswer: "All of the above",
+  },
+
+  {
+    question:
+      " Which of the following command is used to install create-react-app?",
+    incorrectAnswers: [
+      "npm install create-react-app",
+      "npm install -f create-react-app ",
+      "install -g create-react-app ",
+    ],
+    correctAnswer: "npm install -g create-react-app ",
+  },
+  {
+    question: "What of the following is used in React.js to increase performance?",
+    incorrectAnswers: ["Original DOM", "none of the above", "Both A and B."],
+    correctAnswer: "Virtual DOM",
+  },
+  {
+    question: "A class is a type of function, but instead of using the keyword function to initiate it, which keyword do we use??",
+    incorrectAnswers: ["Dataobject", "constructor", "object"],
+    correctAnswer: "class",
+  },
+  {
+    question: " Which of the following acts as the input of a class-based component?",
+    incorrectAnswers: [
+      "class",
+      "factory",
+      "render",
+    ],
+    correctAnswer: "props",
+  },
+
+  {
+    question: "Which of the following keyword is used to create a class inheritance? ",
+    incorrectAnswers: [
+      "create",
+      "inherits",
+      "this",
+    ],
+    correctAnswer: "extends",
+  },
+  {
+    question: " What is the default port where webpack-server runs? ",
+    incorrectAnswers: [
+      "3000",
+      "3030",
+      "6060",
+    ],
+    correctAnswer: "8080",
+  },
+  {
+    question: " How many numbers of elements a valid react component can return? ",
+    incorrectAnswers: [
+      "2",
+      "4",
+      "5",
+    ],
+    correctAnswer: "1",
+  },
+  
+  {
+    question: "  How many ways of defining your variables in ES6 ? ",
+    incorrectAnswers: [
+      "1",
+      "4",
+      "5",
+    ],
+    correctAnswer: "3",
+  },
+  {
+    question: " What are the two ways to handle data in React? ",
+    incorrectAnswers: [
+      "Services & Components ",
+      "State & Services ",
+      "State & Component ",
+    ],
+    correctAnswer: "State & Props",
+  },
+
+];
+
+export default data;
---
+++ React/shoppingcart/src/data.js
@@ -0,0 +1,48 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'apple',
+      price: 200,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnf3kOR45vLzwDNbilt1g8pbeb_TDOmMP-3w&usqp=CAU',
+    },
+    {
+      id: '2',
+      name: 'Mango',
+      price: 240,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPhfSS_YdcHKj7fpTvHL3y5Y-3H67IQN6q8w&usqp=CAU',
+    },
+    {
+      id: '3',
+      name: 'orange',
+      price: 100,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvfAIVPI1PKAlMh2c7n1v3qlOxlGaL48EeIQ&usqp=CAU',
+    },
+    {
+      id: '4',
+      name: 'Banana',
+      price: 60,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXb4XtCXbm-2pcKCNhvfMx8FbP_lVFfEYuoQ&usqp=CAU',
+    },
+    {
+      id: '5',
+      name: 'Grapes',
+      price: 60,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9VKnyMSdzeK5_fi-S0UIe_GcAdcECWSLh-Q&usqp=CAU',
+    },
+    {
+      id: '6',
+      name: 'strawberry',
+      price: 70,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzMyNfL9ltcS7cWnsQiSFg98E9ROzL1iYgpg&usqp=CAU',
+    },
+    {
+      id: '7',
+      name: 'water apple',
+      price: 70,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHycqry0KbijjHDedWnboz5Yibh0_YLmMHLw&usqp=CAU',
+    }
+    
+  ],
+};
+export default data;
